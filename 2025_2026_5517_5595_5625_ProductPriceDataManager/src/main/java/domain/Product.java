567ebebb264d209667103464fac68cc647eee260
gpater <cs05595@uoi.gr>
2025-10-08 21:09:57 +0300

    1) Product, Year classes (To be reviewed!)

--- 2025_2026_5517_5595_5625_ProductPriceDataManager/src/main/java/domain/Product.java
+++ 2025_2026_5517_5595_5625_ProductPriceDataManager/src/main/java/domain/Product.java
@@ -1,5 +1,39 @@
 package domain;
 
-public class Product {
+import java.util.HashMap;
 
+public class Product {
+	private String name; 
+	private String alias;
+	private String category;
+	private HashMap<Year, Integer> years;
+	
+	public Product(String theName, String theAlias, String theCategory) {
+		name = theName;
+		alias = theAlias;
+		category = theCategory;
+		years = new HashMap<>();
+	}
+	
+	public void addYear(Year y, Integer price) {
+		years.put(y, price);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public HashMap<Year, Integer> getYears() {
+		return years;
+	}
+	
+	
 }
---
+++ 2025_2026_5517_5595_5625_ProductPriceDataManager/src/main/java/domain/Year.java
@@ -0,0 +1,12 @@
+package domain;
+import java.util.HashMap;
+
+public class Year {
+	private int id;
+	private HashMap<Product,Integer> yearMap;
+	
+	public Year(int theId) {
+		id = theId;
+	}
+	
+}
